d57e2d10a818aa191fd3d285ef9df6fccce11d72
evabronskayaa <eva27122002b@gmail.com>
2022-06-24 15:27:14 +0500

    token has been hidden

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+from secret import token
+
 HOST = 'https://shop.mango.com/us/'
 URL = 'https://shop.mango.com/us/women/featured/sale-dresses-and-jumpsuits_d19494371'
-TOKEN = '5341385537:AAElipfZFsA9uw4AMOe7prfSMl7gzfIU6jA'
+TOKEN = token
 DRIVER_PATH = 'chromedriver.exe'
