9acc1b3b5f521bf4bcb4caa5cb3a46a37c6bd5a8
evabronskayaa <eva27122002b@gmail.com>
2022-06-24 23:38:18 +0500

    added ui folder

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from secret import token
 
 HOST = 'https://shop.mango.com/us/'
-URL = 'https://shop.mango.com/us/women/featured/sale-dresses-and-jumpsuits_d19494371'
+URL = 'https://shop.mango.com/us/women/dresses-and-jumpsuits_c20249297'
 TOKEN = token
 DRIVER_PATH = 'chromedriver.exe'
